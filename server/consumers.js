396222f9b3259f383cb6aa34a021860b86ff8c95
David Halpin <ddhalpin@gmail.com>
2014-07-18 18:54:35 -0400

    Added MongoDB as well as the client directories

--- server/consumers.js
+++ server/consumers.js
@@ -4,10 +4,17 @@
  * - GET to read
  * - PUT to update
  * - DELETE to delete records.
+ *
+ *  NOTE: Restify ships with support for semver versioning in an 'Accept-Version' header.
  */
 
-//var mongojs = require('mongojs');
-// var db = mongojs('mongodb://dbuser:314159@ds047427.mongolab.com:47427/iserveyou', ['products']);
+
+var mongojs = require('mongojs');
+var databaseUrl = "sqftpro"; // "username:password@example.com/mydb"
+var db = mongojs.connect(databaseUrl, ["consumers"]);
+var ObjectId = mongojs.ObjectId;
+
+//var db = mongojs('mongodb://dbuser:314159@ds047427.mongolab.com:47427/iserveyou', ['products']);
 
 /**
  * Intializes the consumer router to start listenting to message
@@ -17,30 +24,46 @@
  */
 exports.init = function(server) {
 
-	// Redirect the routs for consumers to the relative function.
-	server.get("/v1/consumers", getAllConsumers_);
-	server.post("/v1/consumers", createConsumer_);
-	server.put('/v1/consumers/:id', getConsumer_);
-	server.del('/v1/consumers/:id', removeConsumer_);
+	// Redirect the routes for consumers to the relative function.
+	server.get( {path: "/consumers", version: "1.0.0"}, getAllConsumers_);
+	server.post( {path: "/consumers", version: "1.0.0"}, createConsumer_);
+	server.put( {path: "/consumers/:id",  version: "1.0.0"}, getConsumer_);
+	server.del( {path: "/consumers/:id",  version: "1.0.0"}, removeConsumer_);
 }
 
 
 var getAllConsumers_ = function (req, res, next) {
-	console.log("I am here!!!");
+	
+	// Resitify currently has a bug which doesn't allow you to set default headers
+	// This headers comply with CORS and allow us to server our response to any origin
+	res.header("Access-Control-Allow-Origin", "*"); 
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
+	db.consumers.find(function (err, consumers) {
+		if( err || !consumers) {
+			console.log("No consumers found");
+		}
+   		else {
+
+  // 			consumers.forEach( function(consumer) { console.log(consumer); } );
+			res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
+			res.end(JSON.stringify(consumers));
+		}
+   });
+    
 	return next();
 };
 
 
 var createConsumer_ = function (req, res, next) {
-    // var product = req.params;
-    // db.products.save(product,
-    //     function (err, data) {
-    //         res.writeHead(200, {
-    //             'Content-Type': 'application/json; charset=utf-8'
-    //         });
-    //         res.end(JSON.stringify(data));
-    //     });
+    var consumer = req.params;
+    db.products.save(consumer,
+        function (err, data) {
+            res.writeHead(200, {
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            res.end(JSON.stringify(data));
+        });
     return next();
 };
 
@@ -76,14 +99,16 @@ var getConsumer_ = function (req, res, next) {
 
 
 var removeConsumer_ = function (req, res, next) {
-    // db.products.remove({
-    //     id: req.params.id
-    // }, function (err, data) {
-    //     res.writeHead(200, {
-    //         'Content-Type': 'application/json; charset=utf-8'
-    //     });
-    //     res.end(JSON.stringify(true));
-    // });
+	console.log(req.params.id);
+
+    db.consumers.remove({
+        _id: ObjectId(req.params.id)
+    }, function (err, data) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json; charset=utf-8'
+        });
+        res.end(JSON.stringify(true));
+    });
     return next();
 };
 
